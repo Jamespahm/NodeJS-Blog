82bd819d790e500354e9c2774c423800ab180891
Jamespahm <hoanggfff@gmail.com>
2024-03-31 23:56:18 +0700

    Xây dựng cấu trúc dự án và cài đặt các thư viện cơ bản

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
@@ -5,15 +6,21 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3030;
 
-// import express from 'express';
-// import { engine } from 'express-handlebars';
+// HTTP logger
+app.use(morgan('combined'));
+
+// Use static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', './src/resources/views');
+app.set('views', path.join(__dirname, '/resources/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+app.get('/news', (req, res) => {
+    res.render('news');
 });
 
 app.listen(port, () => {
