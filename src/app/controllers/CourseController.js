0ee9e5abb3e358a264c40b4ce4774f14fde64dd7
Jamespahm <hoanggfff@gmail.com>
2024-04-02 14:45:59 +0700

    [CRUD] Create course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -18,12 +18,33 @@ class CourseController {
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
             .then((course) => {
-                // res.json(course);
-                res.render('course-detail', { course: mongooseToObject(course) });
+                if (course) {
+                    res.render('course/course-detail', { course: mongooseToObject(course) });
+                } else {
+                    // Không tìm thấy khóa học với slug đã cung cấp, chuyển hướng sang trang 404
+                    res.status(404).render('errors/404', { layout: false });
+                }
             })
             .catch((error) => {
                 next(error);
             });
+    }
+
+    // [GET] /course/create
+    create(req, res, next) {
+        res.render('course/create');
+    }
+
+    // [POST] /course/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/maxresdefault.jpg`;
+        const course = new Course(formData);
+        course
+            .save()
+            .then(() => res.redirect('/course'))
+            .catch((error) => {});
+        // res.send('Saved');
     }
 }
 
--- src/routers/course.router.js
+++ src/routers/course.router.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const courseController = require('../app/controllers/CourseController');
 
-router.use('/:slug', courseController.show);
+router.use('/create', courseController.create);
+router.post('/store', courseController.store);
 
+router.use('/:slug', courseController.show);
 router.use('/', courseController.index);
 
 module.exports = router;
