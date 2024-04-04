5842fb4e292308abaef14339478968d55921faa4
Jamespahm <hoanggfff@gmail.com>
2024-04-04 19:56:29 +0700

    [CRUD] Xóa khóa học

--- src/routers/course.router.js
+++ src/routers/course.router.js
@@ -9,6 +9,8 @@ router.post('/store', courseController.store);
 router.get('/:id/edit', courseController.edit);
 router.put('/:id', courseController.update);
 
+router.delete('/:id', courseController.destroy);
+
 router.get('/:slug', courseController.show);
 router.get('/', courseController.index);
 
